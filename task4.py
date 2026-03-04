cadb0075633fc94ada981d0ebc38f5f7dd6d7484
SamiAwawda <samitur02@gmail.com>
2026-03-04 15:11:33 +0300

    Draw a filled yellow rectangle on the image

--- task4.py
+++ task4.py
@@ -4,7 +4,7 @@ img = cv2.imread('images/car.jpg')
 
 start_point=(150, 200)
 end_point=(250, 310)
-color=(0, 255, 0)
+color=(0, 255, 255)
 thickness=-1
 
 cv2.rectangle(img, start_point, end_point, color, thickness)
